a1b900de9ab05d2932a4963df6ffc215cba8cd31
Connor Davenport <connordavenport@Connors-MacBook-Pro.local>
2019-12-02 14:25:18 -0800

    Added User and AuthPayLoad types for authentication

--- src/index.js
+++ src/index.js
@@ -1,44 +1,24 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-// Duummy data
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Full stack tutorial for GraphQL'
-},{
-    id: 'link-1',
-    url: 'connor.com',
-    description: 'test'
-}]
-
-let idCount = links.length
 // 2
 // The revolvers object is the actual inplementation of the GraphQL schema. Its structure 
 // is identical to the structure of the type defined in the typeDefs 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews clone`,
-        feed: () => links,  
-        link: (parent, args) => {
-            var linkID = args.id
-            return links.filter(link => {
-                return link.id == linkID;
-            })[0]
-        }
-        },
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        }, 
+    },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-
-        }
-
-    }
+        post: (root, args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
+                description: args.description
+            })
+        },
+    },
 }
 
 // 3
@@ -47,5 +27,6 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
