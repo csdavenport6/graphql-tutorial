98943e9248c581e3f0abc35cd27e9f8af1c81ee3
Connor Davenport <connordavenport@Connors-MacBook-Pro.local>
2019-12-02 06:47:17 -0800

    added get link query and started adding database support

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-// This defines the GraphQL schema. Here, we define a simple query type with one 
-// field called info, of type string! (the bang indicates that the field should not be null)
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
-
 // Duummy data
 let links = [{
     id: 'link-0',
@@ -27,26 +11,41 @@ let links = [{
     description: 'test'
 }]
 
+let idCount = links.length
 // 2
 // The revolvers object is the actual inplementation of the GraphQL schema. Its structure 
 // is identical to the structure of the type defined in the typeDefs 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews clone`,
-        feed: () => links,
-    },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
+        feed: () => links,  
+        link: (parent, args) => {
+            var linkID = args.id
+            return links.filter(link => {
+                return link.id == linkID;
+            })[0]
+        }
+        },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+
+        }
+
     }
 }
 
 // 3
-// The schema and resolvers are bundled and passed to the GraphQServer, which is imported from graphql-yoga. 
+// The schema (defined in schema.graphql file) and resolvers are bundled and passed to the GraphQServer, which is imported from graphql-yoga. 
 // This tells the server what API operations are accepted and how they should be resolved. 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
